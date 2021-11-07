fa20cbcba61ddad866f6416f98348330201afebd
A.Davarski <42774466+adavarski@users.noreply.github.com>
2021-11-07 17:14:46 +0200

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ variable "hcloud_token" {}
 
 module "demo_cluster" {
   source = "./.."
-  # Can also point to a git repository, e.g. git::https://github.com/StarpTech/k-andy.git?ref=main
+  # Can also point to a git repository, e.g. git::https://github.com/adavarski/k3s-hcloud.git?ref=main
   hcloud_token     = var.hcloud_token
   name             = "demo"
   server_locations = ["nbg1", "fsn1"]
